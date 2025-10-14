fc046715f8bfcae26ac367f7ab69dab0c7d53e95
Jeyson <jeysonlazaroarias12@gmial.com>
2025-10-14 12:12:55 -0500

    subiendo semana9

---
+++ Semana_9/Semana_9.py
@@ -0,0 +1,48 @@
+﻿from triangulo import triangulo
+c = triangulo()
+
+def menu1() -> None:
+    print("BIENVENIDOS AL CALCULO DE AREA Y PERIMETROS")
+    print("********Menu de opciones*********")
+    print("*     1.Triangulo               *")
+    print("*     2.Cuadrado                *")
+    print("*     3.Rectangulo              *")
+    print("*     4.Trapecio                *")
+    print("*     0.Salir                   *")
+    print("*******************************\n")
+
+def menu2() -> int:
+    print("********************")
+    print("*    1.Area        *")
+    print("*    1.Perimetro   *")
+    print("******************\n")
+
+    opcion2 = int(input("Ingrese opcion: "))
+    return opcion2
+
+while True:
+    menu1()
+    while True:
+        opcion = int(input("Ingrese opcion: "))
+        if opcion in (0,1,2,3,4):
+            break
+        else: print("Opcion no valida. Ingrese nuevamente.\n")
+
+    match opcion:
+        case 0: quit() #exit()
+        case 1: 
+            opc2 = menu2()
+            match opc2:
+                case 1: t.area()
+                case 2: t.perimetro()
+
+        case 2: print()
+        case 3: print()
+        case 4: print()
+
+    while True:
+        conti = input("\n¿Desea continuar? (s/n): ")
+        if conti in("s", "n"): break
+        else: print("Error. Presione solo 's' o 'n'. ")
+
+    if conti =="n": break
---
+++ Semana_9/triangulo.py
@@ -0,0 +1,13 @@
+﻿class triangulo:
+    
+    def area(self) -> None:
+        b = int(input("\nIngresa la base: "))
+        h = int(input("Ingresa la altura: "))
+        print("\nArea: ",((b*h)/2))
+
+
+def perimtetro(self) -> None:
+    l1 = int(input("\nIngrese el lado 1: "))
+    l2 = int(input("Ingrese el lado 2: "))
+    l3 = int(input("Ingrese el lado 3: "))
+    print("\nPerimetro: ", (l1+l2+l3))
